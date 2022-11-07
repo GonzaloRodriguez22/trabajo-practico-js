d53dbd266707f0a897079ae4d04b76a7232dd4d0
GonzaloRodriguez22 <104464376+GonzaloRodriguez22@users.noreply.github.com>
2022-11-07 16:01:53 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+//#region 
+var numA =7;
+var numB =9;
+let suma= numA + numB;
+const variables = [1, 5, 4, 7, 3, 9, 2];
+function sumatoria (numA , numB);{
+    var sumatoria = numA + numB;
+    return sumatoria;
+}
+//#endregion
+//#region 
+var numC = 20;
+var numD = 11;
+let Resta = numC - numD;
+const Resta = [6, 3, 10, 11, 19, 4, 14, 2];
+function Resta(numC , numD){
+    var Resta = numC - numD;
+    return Resta;
+}
+//#endregion
+//#region 
+var numE =10;
+var numF =30;
+let Multi = numE * numF;
+const numero= [42, 25, 47, 40, 33, 39, 38];
+function Multi(numE , numF){
+    var Multi = numE * numF;
+    return Multi;
+}
+//#endregion
+//#region 
+var numG =4;
+var numH =6;
+let division = numG / numH;
+const division = [20, 46, 82, 30, 65, 50];
+function division(numG , numH){
+    var division = numG / numH;
+    return division; 
+}
+//#endregion
